b3af7c728d275b3b0c02f23b4ce13b50eae9d506
smabbasht <abbastaqvi81@gmail.com>
2023-05-10 11:59:59 +0500

    update the script for swift-n

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -19,11 +19,11 @@ makepkg -sirc --noconfirm
 yay -S brave-bin --noconfirm
 
 # Install other packages
-sudo pacman -S neovim tmux starship rofi zoxide fzf polybar feh noto-fonts-emoji thunar unzip moreutils flameshot parcellite xclip bat exa man --noconfirm
+sudo pacman -S neovim tmux starship rofi zoxide fzf polybar feh noto-fonts-emoji thunar unzip moreutils flameshot parcellite xclip bat exa man neofetch --noconfirm
 yay -S picom-jonaburg-git --noconfirm
 
 # install other tiny packages
-sudo pacman -S python-pygments imagemagick libcanberra python-pynvim libnotify archlinux-keyring libsecret xorg-xbacklight --noconfirm
+sudo pacman -S python-pygments imagemagick libcanberra python-pynvim libnotify archlinux-keyring libsecret xorg-xbacklight icat --noconfirm
 yay -S find-cursor i3-battery-popup --noconfirm
 
 # Install npm of desired version (downgrade for copilot)
@@ -41,7 +41,8 @@ yay -S sddm-sugar-dark --noconfirm
 # Configure sugar-dark theme
 sudo sed -i 's/Current=.*/Current=sugar-dark/g' /usr/lib/sddm/sddm.conf.d/default.conf
 # Change Background in theme.conf
-sudo sed -i 's/Background=.*/Background=\"~\/.config\/images\/login*\"/g' /usr/share/sddm/themes/sugar-dark/theme.conf
+# sudo sed -i 's/Background=.*/Background=\"~\/.config\/images\/login*\"/g' /usr/share/sddm/themes/sugar-dark/theme.conf
+sudo mv ~/.config/images/login-screen-wall.jpg /usr/share/sddm/themes/sugar-dark/Background.jpg
 # font.capitalization: Font.Capitalize in Components/inputs.qml
 sudo sed -i '/font.capitalization: Font.Capitalize/d' /usr/share/sddm/themes/sugar-dark/Components/Input.qml
 # ForceHideCompletePassword == "true" in theme.conf using sed
@@ -50,6 +51,9 @@ sudo sed -i 's/ForceHideCompletePassword=.*/ForceHideCompletePassword=true/g' /u
 
 # Configure i3 Touchpad through awk and find where it is written Identifier "libinput touchpad catchall"; configure it for scrolling method, natural scrolling, tapping on, tapping button map
 sudo awk '/Identifier "libinput touchpad catchall"/{print;print "        Option \"ScrollMethod\" \"twofinger\"";print "        Option \"NaturalScrolling\" \"true\"";print "        Option \"Tapping\" \"on\"";print "        Option \"TappingButtonMap\" \"lrm\"";next}1' /usr/share/X11/xorg.conf.d/40-libinput.conf > tmp && sudo mv tmp /usr/share/X11/xorg.conf.d/40-libinput.conf
+
+# Install lxappearance to set themes
+sudo pacman -S lxappearance --noconfirm
 
 # Enable Network Manager
 sudo systemctl enable NetworkManager
@@ -64,6 +68,17 @@ rm -rvf Hack.zip
 
 # Installing themes
 yay -S orchis-theme reversal-icon-theme-git breeze-default-cursor-theme --noconfirm
+
+
+# Add the blazingly fast n
+cd
+git clone https://github.com/smabbasht/swift-n
+cd swift-n
+# update fish.config with adding this  in that somewhere "fish_add_path ~/swift-n" using awk or sed 
+awk '/neofetch/{print;print "fish_add_path ~/swift-n";next}1' ~/.config/fish/config.fish > tmp && mv tmp ~/.config/fish/config.fish
+
+# change shell to fish 
+chsh -s /usr/bin/fish
 
 # Running PackerSync for Neovim
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
