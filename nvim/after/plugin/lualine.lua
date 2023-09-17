f9d617ce0146fa18c2669693dc9672b3ff9d6e69
M. Abbas Haider <abbastaqvi81@gmail.com>
2023-09-17 16:56:29 +0500

    misc changes in scr and nvim

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup {
   options = {
-    icons_enabled = false,
-    theme = 'onedark',
+    icons_enabled = true,
+    theme = 'dracula',
     component_separators = '|',
     section_separators = '',
   },
--- nvim/init.lua
+++ nvim/init.lua
@@ -2,12 +2,13 @@
 -- Init.lua
 --------------------------------------------------------------------------------------------------------
 
-
-
 --------------------------------------------------------------------------------------------------------
 -- Set.lua
 --------------------------------------------------------------------------------------------------------
-vim.opt.guifont='Hack Nerd Font'
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.guifont = "Hack Nerd Font"
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -41,7 +42,7 @@ vim.opt.cursorline = true
 
 vim.opt.mouse = ""
 
-vim.g.python3_host_prog = '/usr/bin/python3'
+vim.g.python3_host_prog = "/usr/bin/python3"
 
 vim.g.mapleader = " "
 vim.g.transparent_enabled = true
@@ -49,13 +50,13 @@ vim.g.transparent_enabled = true
 vim.g.surround_no_insert_space = 1
 
 -- [[ Highlight on yank ]]
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = highlight_group,
-    pattern = '*',
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
 })
 
 -- Reduce Alacritty padding when entering Neovim
@@ -68,38 +69,37 @@ vim.cmd([[
   autocmd VimLeave * silent! !sed -i -E '/padding:/,/x: [0-9]+/ s/(x|y): [0-9]+/\1: 8/' ~/.config/alacritty/alacritty.yml | alacritty --daemonize
 ]])
 
-
 --------------------------------------------------------------------------------------------------------
 -- Remap.lua
 --------------------------------------------------------------------------------------------------------
 vim.g.mapleader = " "
 
 -- Source init.lua
-vim.keymap.set('n', '<leader>_', function()
-    vim.cmd('so ~/.config/nvim/init.lua')
-end, { desc = 'source init.lua' })
+vim.keymap.set("n", "<leader>_", function()
+	vim.cmd("so ~/.config/nvim/init.lua")
+end, { desc = "source init.lua" })
 
 -- No-op the arrow keys
-vim.keymap.set({ 'n', 'v', 'i' }, '<Up>', '<nop>')
-vim.keymap.set({ 'n', 'v', 'i' }, '<Down>', '<nop>')
-vim.keymap.set({ 'n', 'v', 'i' }, '<Left>', '<nop>')
-vim.keymap.set({ 'n', 'v', 'i' }, '<Right>', '<nop>')
+vim.keymap.set({ "n", "v", "i" }, "<Up>", "<nop>")
+vim.keymap.set({ "n", "v", "i" }, "<Down>", "<nop>")
+vim.keymap.set({ "n", "v", "i" }, "<Left>", "<nop>")
+vim.keymap.set({ "n", "v", "i" }, "<Right>", "<nop>")
 
 -- map H to 'Hzz' and L to 'Lzz
-vim.keymap.set({ 'n', 'v' }, 'H', 'Hzz')
-vim.keymap.set({ 'n', 'v' }, 'L', 'Lzz')
+vim.keymap.set({ "n", "v" }, "H", "Hzz")
+vim.keymap.set({ "n", "v" }, "L", "Lzz")
 
-vim.keymap.set('n', '<leader>q', vim.cmd.bdelete)
-vim.keymap.set('n', '<leader>l', vim.cmd.bnext)
-vim.keymap.set('n', '<leader>h', vim.cmd.bprevious)
+vim.keymap.set("n", "<leader>q", vim.cmd.bdelete)
+vim.keymap.set("n", "<leader>l", vim.cmd.bnext)
+vim.keymap.set("n", "<leader>h", vim.cmd.bprevious)
 
 -- Browse Files
-vim.keymap.set('n', '<leader>bf', vim.cmd.Ex)
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader>bf", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
 -- Toggle Transparency
-vim.keymap.set('n', '<leader>tt', vim.cmd.TransparentToggle, { silent = true }) -- toggle transparent
-vim.keymap.set("v", "<leader>y", [["+y"<CR>]])                                  -- copy to system clipboard
+vim.keymap.set("n", "<leader>tt", vim.cmd.TransparentToggle, { silent = true }) -- toggle transparent
+vim.keymap.set("v", "<leader>y", [["+y"<CR>]]) -- copy to system clipboard
 -- vim.keymap.set("n", "<leader>y", [["+y"<CR>]]) -- copy to system clipboard
 
 vim.keymap.set("v", "<leader>p", [["_dP]])
@@ -111,8 +111,8 @@ vim.keymap.set({ "n", "v" }, "<leader>op", [[o<esc>p]])
 vim.keymap.set({ "n", "v" }, "<leader>Op", [[O<esc>p]])
 
 -- vim.keymap.set("n", "<leader>s", [[:w<CR>]] ) -- search and replace
-vim.keymap.set("i", "<C-s>", [[<esc>:w<CR>li]])  -- search and replace
-vim.keymap.set("n", "<C-s>", [[:w<CR>]])         -- search and replace
+vim.keymap.set("i", "<C-s>", [[<esc>:w<CR>li]]) -- search and replace
+vim.keymap.set("n", "<C-s>", [[:w<CR>]]) -- search and replace
 
 -- [f]ormat the Buffer
 vim.keymap.set("n", "<F5>", vim.lsp.buf.format)
@@ -126,83 +126,125 @@ vim.keymap.set("n", "B", [[c^]])
 -- Make current file executable
 vim.keymap.set("n", "<leader>x", "[[:!chmod +x %<CR><CR>]]")
 
+vim.keymap.set({ "n", "v", "i" }, "<C-b>", ":NvimTreeToggle<CR>")
+
+-- Navigation
+vim.keymap.set({ "n", "v", "i" }, "<C-h>", "<C-w>h")
+vim.keymap.set({ "n", "v", "i" }, "<C-j>", "<C-w>j")
+vim.keymap.set({ "n", "v", "i" }, "<C-k>", "<C-w>k")
+vim.keymap.set({ "n", "v", "i" }, "<C-l>", "<C-w>l")
+
 --------------------------------------------------------------------------------------------------------
 -- Lazy.lua
 --------------------------------------------------------------------------------------------------------
 local plugins = {
-    'nvim-treesitter/nvim-treesitter',
-    'nvim-treesitter/playground',
-    'mbbill/undotree',
-    'prichrd/netrw.nvim',
-    'olimorris/onedarkpro.nvim',
-    'tpope/vim-fugitive',
-    'lewis6991/gitsigns.nvim',
-    'github/copilot.vim',
-    'xiyaowong/nvim-transparent',
-    'windwp/nvim-autopairs',
-    'tpope/vim-surround',
-    'lervag/vimtex',
-    'numToStr/Comment.nvim',
-    'hoob3rt/lualine.nvim',
-    'kyazdani42/nvim-web-devicons',
-    'neoclide/coc.nvim',
-    'wakatime/vim-wakatime',
-    {
-        'barrett-ruth/live-server.nvim',
-        build = 'yarn global add live-server',
-        config = true
-    },
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
-        dependencies = { { 'nvim-lua/plenary.nvim' } }
-    },
-    -- LSP ZERO
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        dependencies = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            {
-                -- Optional
-                'williamboman/mason.nvim',
-                run = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
-            },
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+	"nvim-treesitter/nvim-treesitter",
+	"nvim-treesitter/playground",
+	"mbbill/undotree",
+	"olimorris/onedarkpro.nvim",
+	"Mofiqul/dracula.nvim",
+	"tpope/vim-fugitive",
+	"lewis6991/gitsigns.nvim",
+	"github/copilot.vim",
+	"xiyaowong/nvim-transparent",
+	"windwp/nvim-autopairs",
+	"tpope/vim-surround",
+	"lervag/vimtex",
+	"numToStr/Comment.nvim",
+	"hoob3rt/lualine.nvim",
+	"nvim-tree/nvim-web-devicons",
+	"neoclide/coc.nvim",
+	"nvim-tree/nvim-tree.lua",
+	"wakatime/vim-wakatime",
+	-- Debugging
+	"mfussenegger/nvim-dap",
+	"rcarriga/nvim-dap-ui",
+	"mfussenegger/nvim-dap-python",
+	"theHamsta/nvim-dap-virtual-text",
+	{
+		"barrett-ruth/live-server.nvim",
+		build = "yarn global add live-server",
+		config = true,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+	},
+	-- plugins by folke
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {},
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {},
+	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+	}, -- LSP ZERO
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{
+				-- Optional
+				"williamboman/mason.nvim",
+				run = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+			},
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' }, -- Required
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'hrsh7th/cmp-nvim-lua' },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lua" },
 
-            -- Snippet
-            { 'L3MON4D3/LuaSnip' }, -- Required
-            { 'rafamadriz/friendly-snippets' },
-
-
-        }
-    }
+			-- Snippet
+			{ "L3MON4D3/LuaSnip" }, -- Required
+			{ "rafamadriz/friendly-snippets" },
+		},
+	},
 }
 
 local opts = {}
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup(plugins, opt)
+require("lazy").setup(plugins, opt)
